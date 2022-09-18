3147792db5aa5d28b2a53af0ec495d4fcc7ab512
sindy <sindy@SINDY_ARCE>
2022-09-17 23:37:25 -0600

    Agregar archivos de proyecto.

---
+++ Resta_Propiedades/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resta_Propiedades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            //Objeto
+            clresta pResta = new clresta();
+            pResta.Nume1 = int.Parse(txtNum1.Text);
+            pResta.Nume2 = int.Parse(txtNum2.Text);
+            textBox3.Text = pResta.resta().ToString();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Clear();
+            txtNum2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
---
+++ Resta_Propiedades/clresta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resta_Propiedades
+{
+    class clresta
+    {
+        //Atributos
+        private int num1;
+        private int num2;
+
+        //Propiedades
+        public int Nume1
+        {
+            get => num1; set => num1 = value;
+        }
+        public int Nume2
+        {
+            get => num2; set => num2 = value;
+        }
+
+        //Método
+        public int resta()
+        {
+            return (num1 - num2);
+        }
+    }
+}
